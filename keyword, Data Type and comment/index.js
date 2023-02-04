987098511f1ba1a955c06f36c79f5126a8e91fcb
rasel726 <md.rasel2726@gmail.com>
2023-02-04 20:29:04 +0600

    Keyword, Data Type and comment

---
+++ keyword, Data Type and comment/index.js
@@ -0,0 +1,12 @@
+document.write("Rasel Sarkar <br/>");
+document.write("0175464388 <br/>");
+document.write("Dhaka, Bangladesh <br/>");
+
+// document.write("Single line comment <br/>");
+
+/*
+Multiple comments
+document.write("Rasel Sarkar <br/>");
+document.write("0175464388 <br/>");
+document.write("Dhaka, Bangladesh <br/>");
+*/
