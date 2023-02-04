ae45fee8c40df20d3486bca5ad6c2a568223176a
rasel726 <md.rasel2726@gmail.com>
2023-02-05 00:29:42 +0600

    add or concatenate strings

---
+++ 7_How to add or concatenate strings/index.js
@@ -0,0 +1,35 @@
+// concatenate = mane jukto kora. (+) er maddhome 2ta string k ek sathe jukto kora jay
+
+document.write("Rasel" + " Sarkar " + "Hridoy <br/>");
+
+// eibar variable er moddhe rekhe try kora jak
+var firstName = "Rasel ";
+var lastName = "Sarkar <br/>";
+
+var fullName = firstName + lastName;
+
+document.write(fullName);
+
+// eibar jinista r ekto onno vabe dekha jak, Ex:1- My name is Rasel Sarkar
+
+var firstName = "Rasel ";
+var lastName = "Sarkar <br/>";
+
+var fullName = firstName + lastName;
+
+document.write("My name is " + fullName);
+
+// Ex:2- Rasel Sarkar is my name 
+var fullName = "Rasel Sarkar";
+document.write(fullName + " is my name <br/>");
+
+// Ex:3- My name is Rasel Sarkar Hridoy
+var fullName = "Rasel Sarkar";
+document.write("My name is " + fullName + " Hridoy <br/>");
+
+// Ex:3- Num1 = 20 and num2 = 30
+var num1 = 20;
+var num2 = 30;
+
+document.write("num1 = " + num1 + " and num2 = " + num2);
+
