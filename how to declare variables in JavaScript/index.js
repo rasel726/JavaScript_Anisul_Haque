f71b8c42e85034351b297a7b2637b173e442864f
rasel726 <md.rasel2726@gmail.com>
2023-02-04 20:53:53 +0600

    declare variables in JavaScript

---
+++ how to declare variables in JavaScript/index.js
@@ -0,0 +1,22 @@
+// Variable
+var name = "Hello wolrd <br/>";
+
+// document.write(name);
+
+name = ("Rasel Sarkar <br/>");
+
+document.write(name);
+
+// camelcase variable name
+var firstName = "Rasel ";
+var lastName = "Sarkar <br/> <br/>";
+
+document.write(firstName, lastName);
+
+// multiple variable in one lines
+var firstName, lastName, phoneNumber;
+firstName = "Name: Rasel ";
+lastName = "Sarkar <br />";
+phoneNumber = "Phone Number: 234567";
+
+document.write(firstName, lastName, phoneNumber);
