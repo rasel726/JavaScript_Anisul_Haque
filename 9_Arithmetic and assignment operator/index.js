1da5ccd40254cc31a3e26e3a2e223a10bf765043
rasel726 <md.rasel2726@gmail.com>
2023-02-05 23:08:45 +0600

    Arithmetic and assignment operator

---
+++ 9_Arithmetic and assignment operator/index.js
@@ -0,0 +1,44 @@
+// Arithmatic operator: +, -, *, /, %(Modulus), **(Exponent), ++(Increment), --(Decrement),
+var num1 = 10;
+var num2 = 5;
+var totalNum = num1 + num2;
+document.write(totalNum, "</br/>");
+
+var num1 = 10;
+var num2 = 5;
+var totalNum = num1 - num2;
+document.write(totalNum, "</br/>");
+
+var num1 = 10;
+var num2 = 5;
+var totalNum = num1 * num2;
+document.write(totalNum, "</br/>");
+
+var num1 = 10;
+var num2 = 5;
+var totalNum = num1 / num2;
+document.write(totalNum, "</br/>");
+
+var num1 = 10;
+var num2 = 3;
+var totalNum = num1 % num2;
+document.write(totalNum, "</br/>");
+
+// Assignment operator: =, +=, -=, *=, /=, %=, **=
+var x = 3;
+x += 6; // x = x + 6
+document.write(x, "<br/>");
+
+var a = 4;
+a *= 4; // a = a * 4;
+document.write(a, "<br/>");
+
+var x = 6;
+x /= 3; // x = x / 6
+document.write(x, "<br/>");
+
+var x = 10;
+x %= 7; // x = x % 6
+document.write(x, "<br/>");
+
+
