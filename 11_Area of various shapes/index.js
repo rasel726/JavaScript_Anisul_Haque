5d42488d8dbfaecda250a6b2925320f7edcf7871
rasel726 <md.rasel2726@gmail.com>
2023-02-07 00:41:53 +0600

    Area of various shapes

---
+++ 11_Area of various shapes/index.js
@@ -0,0 +1,22 @@
+/* 
+// Square er area ber korar formula: Ex-1
+var base = prompt("Enter Base = ");
+var height = prompt("Enter Height = ");
+
+var area = base * height;
+document.write("Area = " + area + "<br/>");
+
+// Ex-2 (ata doshomiker jonno)
+var base = parseFloat(prompt("Enter Base = "));
+var height = parseFloat(prompt("Enter Height = "));
+
+var area = base * height;
+document.write("Area = " + area + "<br/>");
+*/
+
+// Rectangle er area ber korar formula: Ex-1
+var base = parseFloat(prompt("Enter Base = "));
+var height = parseFloat(prompt("Enter Height = "));
+
+var area = (base * height) / 2;
+document.write("Area = " + area + "<br/>");
