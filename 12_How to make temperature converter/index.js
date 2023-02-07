cba9ccc5a2a576a990aafca099e3bc181d4d59a1
rasel726 <md.rasel2726@gmail.com>
2023-02-07 20:48:05 +0600

    How to make temperature and Hour to Minutes converter

---
+++ 12_How to make temperature converter/index.js
@@ -0,0 +1,24 @@
+/*
+// Farenheit to Celcius covert
+var farn = parseFloat(prompt("Enter Farenheit : "));
+
+var cels = (farn - 32) * (5/9);
+
+document.write("Celcius = " + cels);
+*/
+
+/*
+// Celcius to Farenheit covert
+var cels = parseFloat(prompt("Enter Celcius : "));
+
+var farn = (cels * (9/5)) + 32;
+
+document.write("Farenheit = " + farn);
+*/
+
+// Celcius to Minutes covert
+var hour = parseFloat(prompt("Enter Hour : "));
+
+var min = hour * 60;
+
+document.write("Minutes = " + min);
