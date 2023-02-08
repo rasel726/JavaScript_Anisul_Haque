aedf4d3fc5f72f26274d3667599006488e653c2f
rasel726 <md.rasel2726@gmail.com>
2023-02-08 23:45:12 +0600

    added

---
+++ 13_Relational and Logical Operator/index.js
@@ -0,0 +1,29 @@
+// Relational operator: >, >=, <, <=, ==, ===, !=, !==
+var num1 = 20;
+var num2 = 25;
+
+document.write((num1 > num2) + "<br/>"); //num1 er theke num2 boro kina
+
+document.write((num1 >= num2) + "<br/>"); //num1 er theke num2 boro/soman kina
+
+document.write((20 >= 20) + "<br/>"); //20 er theke 20 boro/soman kina
+
+document.write((20 > 20) + "<br/>"); //20 er theke 20 boro kina
+
+document.write((20 < 20) + "<br/>"); //20 er theke 20 choto kina
+
+document.write((20 <= 20) + "<br/>"); // 20 er theke 20 choto/soman kina
+
+document.write((20 == 20) + "<br/>"); // ekhane 2ta man soman kina
+
+document.write((20 == "20") + "<br/>"); //1ta string thakleo er kaj hocche just value check kora. data check kora er kaj noy
+
+document.write((20 === "20") + "<br/>"); //er kaj hocche value and data check kora
+
+document.write((25 != 20) + "<br/>"); //er mane hocche soman noy
+
+
+
+
+
+// Logical operator: &&, ||, !
