662e160aa32cdfd0e6a89b8ba68d77b97e377308
rasel726 <md.rasel2726@gmail.com>
2023-02-10 00:27:31 +0600

    added

---
+++ 14_if, else if, else/index.js
@@ -0,0 +1,25 @@
+// if, else use
+var num = 5;
+
+if(num%2==0){
+    document.write("Even" + "br/");
+}
+
+else{
+    document.write("Odd" + "<br/>");
+}
+
+var num1 = 0;
+
+if(num1 > 0){
+    document.write("Positive" + "<br/>");
+}
+
+if(num1 < 0){
+    document.write("Negative" + "<br/>");
+}
+
+else{
+    document.write("Zero" + "<br/>");
+}
+
