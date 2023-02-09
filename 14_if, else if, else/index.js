04b38610aec1a3f9e4a6627193995003a20807e5
rasel726 <md.rasel2726@gmail.com>
2023-02-10 00:58:59 +0600

    added

--- 14_if, else if, else/index.js
+++ 14_if, else if, else/index.js
@@ -1,4 +1,5 @@
 // if, else use
+// Ex-1
 var num = 5;
 
 if(num%2==0){
@@ -9,6 +10,7 @@ else{
     document.write("Odd" + "<br/>");
 }
 
+// Ex-2
 var num1 = 0;
 
 if(num1 > 0){
@@ -23,3 +25,83 @@ else{
     document.write("Zero" + "<br/>");
 }
 
+// Ex-3
+var num1 = 0;
+
+if(num1 > 0){
+    document.write("Positive" + "<br/>");
+}
+
+if(num1 < 0){
+    document.write("Negative" + "<br/>");
+}
+
+if(num1 == 0){
+    document.write("Zero" + "<br/>");
+}
+
+// if, else if, else
+// Ex-1
+/* 
+var num2 = 5;
+
+if(num2 > 0){
+    document.write("Positive" + "<br/>");
+}
+
+else if(num2 < 0){
+    document.write("Negative" + "<br/>");
+}
+
+else{
+    document.write("Zero" + "<br/>");
+}
+
+// Ex-2
+var num3 = prompt("Enter a number : ");
+
+if(num3 > 0){
+    document.write("Positive" + "<br/>");
+}
+
+else if(num3 > 0){
+    document.write("Positive" + "<br/>");
+}
+
+else{
+    document.write("Zero" + "<br/>");
+}
+*/
+
+
+// Task-4
+// Grade System (marks to grade)
+var marks = prompt("Enter your marks : ");
+
+if(marks >= 80){
+    document.write("Result : " + "A+" + "<br/>");
+}
+
+else if(marks >= 70){
+    document.write("Result : " + "A" + "<br/>");
+}
+
+else if(marks >= 60){
+    document.write("Result : " + "A-" + "<br/>");
+}
+
+else if(marks >= 50){
+    document.write("Result : " + "B" + "<br/>");
+}
+
+else if(marks >= 40){
+    document.write("Result : " + "C" + "<br/>");
+}
+
+else if(marks >= 33){
+    document.write("Result : " + "D" + "<br/>");
+}
+
+else{
+    document.write("Result : " + "Failed" + "<br/>");
+}
