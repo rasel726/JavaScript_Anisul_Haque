4256ea90e38be059119b3a3f30157c55c61a9dbb
rasel726 <md.rasel2726@gmail.com>
2023-02-10 23:59:50 +0600

    Letter Grade Program

---
+++ 15_Letter Grade Program/index.js
@@ -0,0 +1,25 @@
+// Task-4
+// Letter Grade Program
+var marks = prompt("Enter your marks : ");
+
+var result = "";
+
+if(marks > 100){
+    result = "Invalid";
+} else if(marks >= 80){
+    result = "5 (A+)";
+} else if(marks >= 70){
+    result = "4 (A)";
+} else if(marks >= 60){
+    result = "3.5 (A-)";
+} else if(marks >= 50){
+    result = "3 (B)";
+} else if(marks >= 40){
+    result = "2 (C)";
+} else if(marks >= 33){
+    result = "1 (D)";
+} else{
+    result = "Failed";
+}
+
+document.write("Result : GPA- " + result + "<br/>");
