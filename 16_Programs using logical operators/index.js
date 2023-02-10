410ec31a898e3f4ad5f088d1c2cfdec75f4a605f
rasel726 <md.rasel2726@gmail.com>
2023-02-11 00:48:56 +0600

    added

---
+++ 16_Programs using logical operators/index.js
@@ -0,0 +1,44 @@
+/*
+// Task-4
+// Letter Grade Program
+var marks = prompt("Enter your marks : ");
+
+var result = "";
+
+if(marks > 100 || marks < 0){
+    result = "Invalid Marks";
+}else if(marks >= 80){
+    result = "5 (A+)";
+} else if(marks >= 70){
+    result = "4 (A)";
+} else if(marks >= 60){
+    result = "3.5 (A-)";
+} else if(marks >= 50){
+    result = "3 (B)";
+} else if(marks >= 40){
+    result = "2 (C)";
+} else if(marks >= 33){
+    result = "1 (D)";
+} else{
+    result = "Fail";
+}
+
+document.write("Result : GPA- " + result + "<br/>");
+*/
+
+// 3ti songkha nibo and 3ti songkhar moddhe boro songkhati nirnoy korar program
+var num1 = prompt("Enter number 1 : ");
+var num2 = prompt("Enter number 2 : ");
+var num3 = prompt("Enter number 3 : ");
+
+var bigNumber = "";
+
+if(num1 > num2 && num1 > 3){
+    bigNumber = num1;
+} else if(num2 > num1 && num2 > num3){
+    bigNumber = num2;
+} else{
+    bigNumber = num3;
+}
+
+document.write("Big number = " + bigNumber);
