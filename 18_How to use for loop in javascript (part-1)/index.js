a45a3b48fc71aaf197456db4ad7a85c445e97013
Rasel Sarkar <md.rasel2726@gmail.com>
2023-02-11 15:25:07 +0600

    added

--- 18_How to use for loop in javascript (part-1)/index.js
+++ 18_How to use for loop in javascript (part-1)/index.js
@@ -1,7 +1,20 @@
-// for loop use 
+// for loop use
+/*
+// ex-1
 for(var x = 1; x <= 5; x++){
     document.write("<h1>Bangladesh</h1>");
 }
 
 document.write("<h1>End</h1>");
+
+*/
+
+// ex-2: 100 porjonto print
+for(var x = 1; x <= 100; x++){
+    document.write(" " + x);
+}
+
+document.write("<h1>End</h1>");
+
+
 
