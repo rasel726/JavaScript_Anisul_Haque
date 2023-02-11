a0b00ff168a76cce03fe1e5afaf532206cfa52cb
rasel726 <md.rasel2726@gmail.com>
2023-02-12 00:21:59 +0600

    added

--- 19_How to use for loop in javascript (part-2)/index.js
+++ 19_How to use for loop in javascript (part-2)/index.js
@@ -1,35 +1,48 @@
-/*
-// 1-10 porjonto songkhagulor jogfol
+// 1-5 porjonto songkhagulo jog korbo
 var sum = 0;
-
-for(var x = 1; x <= 10; x = x + 1){
+for(var x = 1; x <= 5; x = x+1){
     sum = sum + x;
+
 }
 
-document.write(sum);
-*/
+// document.write(sum);
 
-/*
-// 5-10 porjonto songkhagulor jogfol
+
+
+// 5-10 porjonto songkhagulo jog korbo
 var sum = 0;
-
-for(var x = 5; x <= 10; x = x + 1){
+for(var x = 5; x <= 10; x = x+1){
     sum = sum + x;
+
 }
 
-document.write(sum);
-*/
+// document.write(sum);
 
-// user theke input niye songkhagulor jogfol
+
+/*
+// eibar user kache 2ta input nibo and tar moddhe thaka songkhagulor jogfol korbo
 var m = parseInt(prompt("Enter starting number : "));
 var n = parseInt(prompt("Enter ending number : "));
 
-
 var sum = 0;
 
-for(var x = m; x <= n; x = x + 1){
+for(x = m; x <= n; x = x + 1){
     sum = sum + x;
 }
 
 document.write(sum);
+*/
+
+
+// eibar ekta loop amra 5 bar chalabo
+
+for(var x = 1; x <= 5; x = x + 1){
+    var num1 = parseInt(prompt("Enter first number : "));
+    var num2 = parseInt(prompt("Enter second number : "));
+    var sum = num1 + num2;
+    document.write("Result = " + sum + "<br/>");   
+}
+
+
+
 
