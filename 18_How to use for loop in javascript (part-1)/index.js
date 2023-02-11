88c73aacf2a70c551a0ac9d945f2d9f1a54e7ac7
Rasel Sarkar <md.rasel2726@gmail.com>
2023-02-11 16:25:52 +0600

    for loop complete

--- 18_How to use for loop in javascript (part-1)/index.js
+++ 18_How to use for loop in javascript (part-1)/index.js
@@ -9,12 +9,42 @@ document.write("<h1>End</h1>");
 
 */
 
+/*
 // ex-2: 100 porjonto print
 for(var x = 1; x <= 100; x++){
     document.write(" " + x);
 }
 
 document.write("<h1>End</h1>");
+*/
 
 
+/*
+// ex-3: 100 porjonto bejor songkhagulo print
 
+for(var x = 1; x <= 99; x = x + 2){
+    document.write(" " + x);
+}
+
+document.write("<h1>End</h1>");
+*/
+
+
+/*
+// ex-4: 100 porjonto jor songkhagulo print
+
+for(var x = 2; x <= 100; x = x + 2){
+    document.write(" " + x);
+}
+
+document.write("<h1>End</h1>");
+*/
+
+
+// ex-5: 10-1 porjonto print korar program
+
+for(var x = 10; x >= 1; x = x - 1){
+    document.write(" " + x);
+}
+
+document.write("<h1>End</h1>");
