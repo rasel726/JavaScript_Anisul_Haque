a95af5a87daf7f6cb27fcef00869505f2fe7dc58
Rasel Sarkar <md.rasel2726@gmail.com>
2023-02-11 16:58:51 +0600

    added

---
+++ 19_How to use for loop in javascript (part-2)/index.js
@@ -0,0 +1,35 @@
+/*
+// 1-10 porjonto songkhagulor jogfol
+var sum = 0;
+
+for(var x = 1; x <= 10; x = x + 1){
+    sum = sum + x;
+}
+
+document.write(sum);
+*/
+
+/*
+// 5-10 porjonto songkhagulor jogfol
+var sum = 0;
+
+for(var x = 5; x <= 10; x = x + 1){
+    sum = sum + x;
+}
+
+document.write(sum);
+*/
+
+// user theke input niye songkhagulor jogfol
+var m = parseInt(prompt("Enter starting number : "));
+var n = parseInt(prompt("Enter ending number : "));
+
+
+var sum = 0;
+
+for(var x = m; x <= n; x = x + 1){
+    sum = sum + x;
+}
+
+document.write(sum);
+
