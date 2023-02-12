540ec5ecee01dc90196946f7db2a9ee85ac48402
rasel726 <md.rasel2726@gmail.com>
2023-02-13 01:04:47 +0600

    added

---
+++ 20_How to use while loop in javascript/index.js
@@ -0,0 +1,29 @@
+// while loop
+var x = 1;
+
+while(x <= 5){
+    // document.write(" " + x);
+    x = x + 1;
+}
+
+// 1-10 porjonto songkhagulor jogfol
+var i = 1;
+var sum = 0;
+
+while(i <= 10){
+    sum = sum + i;
+    i = i + 1;
+}
+
+// document.write(sum);
+
+// 1-100 porjonto jor songkhagulor jogfol
+var i = 2;
+var sum = 0;
+
+while(i <= 100){
+    sum = sum + i;
+    i = i + 2;
+}
+
+document.write(sum);
