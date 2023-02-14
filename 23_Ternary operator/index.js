4aef58fe40aec3bf315428fcbfd661c206d9a0f5
rasel726 <md.rasel2726@gmail.com>
2023-02-15 00:51:26 +0600

    ternary operator

---
+++ 23_Ternary operator/index.js
@@ -0,0 +1,14 @@
+// ternary condition
+/*
+// 2ti songkhar jonno ata
+var number = Number(prompt("Enter a number : "));
+
+var result = number > 0 ? "Positive" : "Negative";
+document.write(result);
+*/
+
+
+// jodi 1 er odhik hoy sekhetre ki hobe
+var number = Number(prompt("Enter a number : "));
+var result = number > 0 ? "Possitive" : number < 0 ? "Negative" : "Zero";
+document.write(result);
