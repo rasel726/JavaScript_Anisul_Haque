575377b48481af8e8833e49efa307dc803c272b7
rasel726 <md.rasel2726@gmail.com>
2023-02-17 00:59:06 +0600

    added

--- 25_IIFEs and function expression/index.js
+++ 25_IIFEs and function expression/index.js
@@ -5,10 +5,18 @@
 
 // Function expression
 const display2 = function displayMessage(msg) {
-    document.write(msg);
+    // document.write(msg);
 }
 
 display2('I am message!');
 
 // ( ), { }, [ ], < >
 // Task-7: Creat an IIFEs that print sum of 2 number
+const sum = function numbers() {
+    var num1 = 10;
+    var num2 = 20;
+    var result = num1 + num2;
+    document.write(result);
+}
+
+sum();
