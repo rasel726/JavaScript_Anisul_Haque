fc702767304929cacdfcf076ce8ffc1ebd28e3cb
Rasel Sarkar <md.rasel2726@gmail.com>
2023-02-16 17:35:09 +0600

    IIFEs and function expression

---
+++ 25_IIFEs and function expression/index.js
@@ -0,0 +1,14 @@
+// // IIFES (Immediately Invokeable Function Expression)
+// (function display(message) {
+//     document.write(message);
+// })("Hello Bangladesh");
+
+// Function expression
+const display2 = function displayMessage(msg) {
+    document.write(msg);
+}
+
+display2('I am message!');
+
+// ( ), { }, [ ], < >
+// Task-7: Creat an IIFEs that print sum of 2 number
