5c1ff0eb27da53ed1f018cf263582e7a4179f153
rasel726 <md.rasel2726@gmail.com>
2023-02-20 23:25:02 +0600

    One dimensional array

---
+++ 29_One dimensional array/index.js
@@ -0,0 +1,15 @@
+// one dimensional array
+
+function highestScore(scores) {
+    var max = scores[0];
+    for (var x = 0; x < scores.length; x++) {
+        if (max < scores[x]) {
+            max = scores[x];
+        }
+    }
+    return max;
+}
+
+var scores = [21, 28, 1, 88, 15];
+var maxScore = highestScore(scores);
+console.log(maxScore);
