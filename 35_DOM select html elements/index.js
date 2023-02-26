8710557815bab6a6e075d037059064f249d78cb6
rasel726 <md.rasel2726@gmail.com>
2023-02-26 21:15:40 +0600

    added

---
+++ 35_DOM select html elements/index.js
@@ -0,0 +1,23 @@
+// getElmentById
+
+// document.getElementById("heading1").innerHTML = "Hello";
+
+
+// ata variable er vitoreo rakhte pari
+var myHeading = document.getElementById("heading1");
+myHeading.innerHTML = "Hello";
+
+var myHeading = document.getElementById("heading2");
+myHeading.innerHTML = "Good Bye Good Bye";
+
+
+document.getElementById("para").innerHTML = "Bye";
+
+
+// getElementByTagName
+document.getElementsByTagName("h3")[0].innerHTML = "Hi";
+document.getElementsByTagName("h3")[1].innerHTML = "Bye";
+
+
+
+// getElementByClassName
