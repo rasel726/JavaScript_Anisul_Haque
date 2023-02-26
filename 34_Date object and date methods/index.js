f61305393e64bd485566b1adaf3da37c45c13e7b
rasel726 <md.rasel2726@gmail.com>
2023-02-26 20:34:21 +0600

    Date object and date methods

---
+++ 34_Date object and date methods/index.js
@@ -0,0 +1,21 @@
+// Date Object
+var date = new Date();
+console.log(date);
+
+var year = date.getFullYear();
+console.log(year);
+
+var year = date.getMonth();
+console.log(year);
+
+var currentDate = date.getDate();
+console.log(currentDate);
+
+var currentDay = date.getDay();
+console.log(currentDay);
+
+var currentHour = date.getHours();
+console.log(currentHour);
+
+var currentMinutes = date.getMinutes();
+console.log(currentMinutes);
