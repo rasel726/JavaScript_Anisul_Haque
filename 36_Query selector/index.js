0a8b73b508635e0fb67f87c9e152e5c97009dc30
rasel726 <md.rasel2726@gmail.com>
2023-03-01 20:31:11 +0600

    added

--- 36_Query selector/index.js
+++ 36_Query selector/index.js
@@ -1,7 +1,10 @@
-// querySelector() use
+/* // querySelector() use
 
 document.querySelector("#pid").innerHTML = "Hello"; // id er jonno # use korte hobe
 document.querySelector(".bd").innerHTML = "Change to India"; // class er jonno # use korte hobe
 document.querySelector("p").innerHTML = "Tamal"; // tag er jonno jsut tag name call korte hobe
+ */
 
-
+document.querySelector("a").innerHTML = "Home to Menu";
+document.querySelector("li a").innerHTML = "Tutorial to Video";
+document.querySelector(".my-div").innerHTML = "Contact to Number";
